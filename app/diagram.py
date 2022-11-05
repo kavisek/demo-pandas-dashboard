19958766632b936b6b3795a6a7cfede5fec3a526
kavisek <kavi.skhon@gmail.com>
2022-11-04 22:58:37 -0700

    feat: adding diagrams 2

--- app/diagram.py
+++ app/diagram.py
@@ -8,8 +8,8 @@ OUTPUT_DIRECTORY = "./output"
 with Diagram(
     "Pandas Dashboard", filename=f"{OUTPUT_DIRECTORY}/architecture", show=False
 ):
-    cache = Redis("Producer")
-    producer = Node("Redis", shape="box")
+    cache = Redis("Redis")
+    producer = Node("Producer App", shape="box")
     consumer = Node("FastAPI App", shape="box")
 
     producer >> cache >> consumer
